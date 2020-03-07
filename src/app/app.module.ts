fe144d9aeec0adb0f1aef64b9496aea31adcb8ce
EDUARDO DE JESUS OLIVER ROMAN <olivereduardoroman@gmail.com>
2020-03-06 18:13:48 -0600

    1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AppRoutingModule } from './app-routing.module';
+import { MenuComponent } from './components/menu/menu.component';
+//import { AboutComponent } from './pages/about/about.component';
+//import { ContactComponent } from './pages/contact/contact.component';
+//import { HomeComponent } from './pages/home/home.component';
+import { PagesModule } from './pages/pages.module';
+
+import {HttpClientModule} from '@angular/common/http';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //AboutComponent,
+    //ContactComponent,
+    //HomeComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
